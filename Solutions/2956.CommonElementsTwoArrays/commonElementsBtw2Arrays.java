fffe3c97dfe79b0278feceee0ad7b2074f1ef3d3
AlexDevThings <alexheavy1@gmail.com>
2025-02-06 12:48:24 +0100

    feat: ✨ Problem 2956 solved
    Problem 11 solved thekey it's using HashSet

---
+++ Solutions/2956.CommonElementsTwoArrays/commonElementsBtw2Arrays.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        int count1 = 0;
+        int count2 = 0;
+        int ans1;
+        int ans2;
+
+        for(int num:nums1){
+            set1.add(num);
+        }
+        for(int num:nums2){
+            set2.add(num);
+        }
+
+        for (int num : nums1) {
+            if (set2.contains(num)) {
+                count1++;
+            }
+        }
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                count2++;
+            }
+        }
+        
+        return new int[]{count1, count2};
+    }
+}
