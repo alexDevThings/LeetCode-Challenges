91dac88e26fd0c055a7b9c3f827e87cc51b8fbe8
AlexDevThings <alexheavy1@gmail.com>
2025-02-07 18:00:12 +0100

    feat: ✨ Problem 9 solved
    Problem 9 solved it's more efficient to just invert half number and compare

---
+++ Solutions/palindromeNumber_9/Solution.java
@@ -0,0 +1,14 @@
+package Solutions.palindromeNumber_9;
+public class Solution {
+    public boolean isPalindrome(int x) {
+        int revertedNumber = 0, temp = 0;
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        while (x > revertedNumber) {
+            temp = x % 10;
+            revertedNumber = revertedNumber * 10 + temp;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
