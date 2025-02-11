759d47b265ebba72ac6b7c6b1c7eecaa94c37ace
AlexDevThings <alexheavy1@gmail.com>
2025-02-11 23:38:05 +0100

    refactor: :fire: remove unnecessary pointers

--- Solutions/AddTwoNumbers_2/Solution.java
+++ Solutions/AddTwoNumbers_2/Solution.java
@@ -1,7 +1,5 @@
 package Solutions.sumTwoNumbers_2;
 
-import java.util.List;
-
 public class Solution {
     public class ListNode {
          int val;
@@ -11,24 +9,20 @@ public class Solution {
          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }    
 
-
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
-        ListNode currentl1 = l1; 
-        ListNode currentl2 = l2;
         int carry = 0, sum = 0;
 
-        while (currentl1 != null || currentl2 != null) {
+        while (l1 != null || l2 != null) {
             sum = carry;
-            System.out.println(current.val);
-            if (currentl1 != null) {
-                sum += currentl1.val;
-                currentl1 = currentl1.next;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-            if (currentl2 != null) {
-                sum += currentl2.val;
-                currentl2 = currentl2.next;
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
             }
 
             if (sum > 9) {
@@ -37,17 +31,13 @@ public class Solution {
             } else {
                 carry = 0;  
             }
-
             current.val = sum;
-            System.out.println(current.val);
 
-            if (currentl1 != null || currentl2 != null) {
+            if (l1 != null || l2 != null) {
                 current.next = new ListNode(sum % 10);
                 current = current.next;
                 System.out.println(current.val);
             }
-            System.out.println(current.val);
-
         }
 
         if(carry > 0){
